8c53d35227467b124015b2c668b81979717f2268
Manish <nayakmanish65819@gmail.com>
2025-03-26 23:50:13 +0530

    init

---
+++ main.js
@@ -0,0 +1,13 @@
+function greet(name) {
+    return "bete, " + name;
+}
+
+let ans = greet("nayak");
+let ans2 = greet("timmu");
+let ans3 = greet("simmu");
+
+
+console.log(ans);
+console.log(ans2);
+console.log(ans3)
+gir
