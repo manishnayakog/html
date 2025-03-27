8ece6e51c21335dd7be435a713981962b6936a74
Manish <nayakmanish65819@gmail.com>
2025-03-28 01:26:05 +0530

    added new object program in loop

--- object.js
+++ object.js
@@ -1,8 +1,18 @@
-function greet(user){
-    console.log("Hii "+ user.name + " your age is " + user.age);
+
+function greet(user) {
+  console.log(
+    `${user.gender == "Male" ? "MR." : "MRS."} ${user.firstname} ${
+      user.lastname
+    } your age is ${user.age} and you ${
+      user.age >= 18 ? "can vote" : "cannot vote"
+    }`
+  );
 }
+
 let user = {
-    name: "Manish",
-    age: 20
-}
+  firstname: "Manish",
+  age: 20,
+  lastname: "Nayak",
+  gender: "Male",
+};
 greet(user);
