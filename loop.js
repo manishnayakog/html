6d8e5d1143c3d310385faf47c99d310daae6dde1
Manish <nayakmanish65819@gmail.com>
2025-03-28 00:07:38 +0530

    added loop function

---
+++ loop.js
@@ -0,0 +1,19 @@
+// let users = ["mohan", "radhe","sohan","seerat"]
+// console.log(users[0])
+// console.log(users[1])
+// console.log(users[2])
+// console.log(users.length)
+// for(i=0;i<users.length;i++){
+//     console.log(users[i]
+//     )
+// }
+
+let num = 10
+let sum = 0
+for (i=1;i<=10;i++){
+    sum = i+sum
+    console.log("cureent value sum ="+ sum)  
+}
+
+
+console.log(sum)
--- main.js
+++ main.js
@@ -1,13 +1,24 @@
-function greet(name) {
-    return "bete, " + name;
-}
+// function sum(a b) {
+//    let totalsum = a + b;
+//        return totalsum; 
+// }
 
-let ans = greet("nayak");
-let ans2 = greet("timmu");
-let ans3 = greet("simmu");
+// let ans = sum(3,4);
+// let ans2 = sum(4,5);
+// let ans3 = sum(6,4);
 
 
-console.log(ans);
-console.log(ans2);
-console.log(ans3)
-gir
+// console.log(ans);
+// console.log(ans2);
+// console.log(ans3)
+
+// function canvote (age){
+//     if (age > 18) {
+//         return true;
+//      } else{
+//         return false;
+//      }
+// }
+//  let ans = canvote(19);
+
+// console.log(ans); 
