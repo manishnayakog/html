8cc416e43460ba1436a49ea2c85088754972b604
Manish <nayakmanish65819@gmail.com>
2025-03-28 23:34:07 +0530

    addded Sum os two number

--- object.js
+++ object.js
@@ -1,18 +1,11 @@
 
-function greet(user) {
-  console.log(
-    `${user.gender == "Male" ? "MR." : "MRS."} ${user.firstname} ${
-      user.lastname
-    } your age is ${user.age} and you ${
-      user.age >= 18 ? "can vote" : "cannot vote"
-    }`
-  );
+function sum(n) {
+  let ans = 0;
+  for (let i = 1; i <= n; i++) {
+    ans = ans + i;
+  }
+  return ans;
 }
 
-let user = {
-  firstname: "Manish",
-  age: 20,
-  lastname: "Nayak",
-  gender: "Male",
-};
-greet(user);
+const ans = sum(100);
+console.log(ans);
