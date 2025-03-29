efaadf3e4a84aa6ad979a6296a150a32381bc8b8
Manish <nayakmanish65819@gmail.com>
2025-03-29 23:48:15 +0530

    added asynchoronous function

--- main.js
+++ main.js
@@ -1,39 +1,15 @@
-// function sum(a b) {
-//    let totalsum = a + b;
-//        return totalsum; 
-// }
+const fs = require("fs");
+console.log("log1");
 
-// let ans = sum(3,4);
-// let ans2 = sum(4,5);
-// let ans3 = sum(6,4);
+// const contents = fs.readFile("a.txt", "utf-8");
+// console.log(contents);
 
+fs.readFile("b.txt", "utf-8", function (err, data) {
+  if (err) {
+    console.log("There is a error");
+  } else {
+    console.log("data:", data);
+  }
+});
 
-// console.log(ans);
-// console.log(ans2);
-// console.log(ans3)
-
-// function canvote (age){
-//     if (age > 18) {
-//         return true;
-//      } else{
-//         return false;
-//      }
-// }
-//  let ans = canvote(19);
-
-// console.log(ans); 
-
-function sum(n) {
-	let ans = 0;
-	for (let i = 1; i <= n; i++) {
-		ans = ans + i
-	}
-	return ans;
-}
-
-const ans1 = sum(100);
-console.log(ans1);
-const ans2 = sum(1000);
-console.log(ans2);
-const ans3 = sum(10000);
-console.log(ans3);
+console.log("log2");
